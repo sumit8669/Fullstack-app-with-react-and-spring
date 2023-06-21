16b29681eb0d65ac27ebe5e48eae6423665b1b73
Sumit Rana <sumit2628r@gmail.com>
2023-06-21 22:25:51 +0530

    updated commit

--- employee-managment-app/src/App.js
+++ employee-managment-app/src/App.js
@@ -9,16 +9,16 @@ function App() {
   return (
     <div>
       <Router>
-             <Header />
-              <div className="container">
-                   <Routes>
-                         <Route path = "/" exact element={<ListEmployee/>}></Route>
-                         <Route path = "/employee" element={<ListEmployee/>}></Route>
-                         <Route path = "/add-employee" element={<CreateEmployee/>}></Route>
-                   </Routes>
-              </div>
-              <Footer/>
-     </Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/"  element={<ListEmployee />}></Route>
+            <Route path="/employee" element={<ListEmployee />}></Route>
+            <Route path="/add-employee" element={<CreateEmployee />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- employee-managment-app/src/components/ListEmployee.jsx
+++ employee-managment-app/src/components/ListEmployee.jsx
@@ -1,56 +1,54 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import EmployeeService from '../services/EmployeeService'
 
 export default class ListEmployee extends Component {
- constructor(props){super(props)
-                this.state ={
-                    employees:[]
-                }
-                this.addEmployee = this.addEmployee.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      employees: []
+    }
   }
-  componentDidMount(){
-    EmployeeService.getEmployee().then((res) =>{
-      this.setState({employees : res.data})
+  componentDidMount() {
+    EmployeeService.getEmployee().then((res) => {
+      this.setState({ employees: res.data })
     })
   }
 
-  addEmployee(){
-    this.props.history.push('/add-employee');
-  }
+
 
   render() {
     return (
       <div>
         <h2 className='text-center'>ListEmployee</h2>
         <div className='row'>
-          <button className='btn btn-primary' onClick={this.addEmployee}>Add Employee</button>
-        </div>
+                    <Link to='/add-employee' className='btn btn-primary'>Add Employee</Link>
+                </div>
         <div className='row'>
-            <table className='table table-striped table-bordered'>
-                <thead>
-                    <tr>
-                        <th>Employee Name</th>
-                        <th>Employee Email</th>
-                        <th>Employee Department</th>
-                        <th>Employee Salary</th>
-                        <th>Actions</th>
+          <table className='table table-striped table-bordered'>
+            <thead>
+              <tr>
+                <th>Employee Name</th>
+                <th>Employee Email</th>
+                <th>Employee Department</th>
+                <th>Employee Salary</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.employees.map(
+                  employees =>
+                    <tr key={employees.id}>
+                      <td>{employees.empName}</td>
+                      <td>{employees.email}</td>
+                      <td>{employees.department}</td>
+                      <td>{employees.salary}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    {
-                      this.state.employees.map(
-                        employees =>
-                          <tr key={employees.id}>
-                            <td>{employees.empName}</td>
-                            <td>{employees.email}</td>
-                            <td>{employees.department}</td>
-                            <td>{employees.salary}</td>
-                          </tr>
-                      )
-                    }
-                </tbody>
-            </table>
+                )
+              }
+            </tbody>
+          </table>
 
         </div>
       </div>
